ff64dcd3c1c3e3ebc7fe49777ac2829080cce8eb
Valerii Fedorovych <valera.fedorovich.2000@gmail.com>
2024-03-27 09:51:01 +0200

    initial commit

---
+++ src/main/java/org/company/aircompaniesmanager/AirCompaniesManagerApplication.java
@@ -0,0 +1,13 @@
+package org.company.aircompaniesmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirCompaniesManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirCompaniesManagerApplication.class, args);
+	}
+
+}
